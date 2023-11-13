3bfff3f6509319f6e61d292f2906da689f7bc300
sevans <sevans@highdesertmilk.com>
2023-11-13 14:56:11 -0700

    Done

--- hello-world/src/index2.ts
+++ hello-world/src/index2.ts
@@ -59,3 +59,25 @@ function greet (name: string | null) {
 }
 // TS doesn't like this because you can't call toUpperCase() on a null or undefined object
 greet (null)
+
+//Null Checks
+type Customer = {
+    birthday: Date
+}
+
+function getCustomer(id: number): Customer | null | undefined{
+    return id === 0 ? null: { birthday: new Date() }
+}
+                        // 0 for undefined
+let customer = getCustomer(1)
+// if (customer !== null && customer !== undefined)
+
+// Optional property access operator "?."
+console.log(customer?.birthday) // this only gets executed when there's a customer that's not null, or undefined.
+
+// Optional element acces operator, (this is useful when dealing with arrays)
+// customers?.[0]
+
+// Optional call operator
+let log: any = null
+log?.('a') // this will only get called if log is referencing an actual function, otherwise you get undefined. 

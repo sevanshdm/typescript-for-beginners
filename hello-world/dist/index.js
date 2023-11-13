ed1b4c2580ae43278b578f6e21263ee1d39d9a01
sevans <sevans@highdesertmilk.com>
2023-11-13 09:22:34 -0700

    finished section

--- hello-world/dist/index.js
+++ hello-world/dist/index.js
@@ -8,4 +8,12 @@ function render(document) {
 }
 let numbers = [1, 2, 3];
 let user = [1, 'Shrek'];
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Medium;
+console.log(mySize);
 //# sourceMappingURL=index.js.map
--- hello-world/src/index.ts
+++ hello-world/src/index.ts
@@ -13,3 +13,8 @@ let numbers: number[] = [1, 2, 3]
 
 // Tuple
 let user: [number, string] = [1, 'Shrek']
+
+// enum
+const enum Size { Small = 1, Medium, Large }
+let mySize: Size = Size.Medium
+console.log(mySize)
